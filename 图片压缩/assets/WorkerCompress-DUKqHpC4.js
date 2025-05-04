f984c04d0ac3e61be899438242ed8f0b569fa3e5
Ethaniel <Ethaniel@100713.xyz>
2025-05-04 14:27:51 +0800

    添加图片压缩

---
+++ 图片压缩/assets/WorkerCompress-DUKqHpC4.js
@@ -0,0 +1,1 @@
+function e(r){return new Worker("/assets/WorkerCompress-Cir7qkug.js",{name:r==null?void 0:r.name})}export{e as default};
---
+++ 图片压缩/assets/WorkerPreview-DocWWytt.js
@@ -0,0 +1,1 @@
+function r(e){return new Worker("/assets/WorkerPreview-CJ5dmN4q.js",{name:e==null?void 0:e.name})}export{r as default};
